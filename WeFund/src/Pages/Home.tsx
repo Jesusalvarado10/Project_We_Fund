6615e90d9c40031545ffd9c208df8a814e9ef303
maridanielac <113387123+maridanielac@users.noreply.github.com>
2024-06-01 18:23:23 -0400

    Listo Navbar

--- WeFund/src/Pages/Home.tsx
+++ WeFund/src/Pages/Home.tsx
@@ -4,12 +4,23 @@ import foto from '../assets/fotos.jpg'
 function Home() {
     return (
       <>
-        <NavBar />
+        <div className="fixed top-0 left-0 w-full z-10">
+          <NavBar />
+        </div>
         <img src={foto} alt="background" className="fixed top-0 left-0 w-screen h-screen z-[-1] object-cover" />
+        <div className="relative z-10" style={{ marginTop: '100vh', backgroundColor: 'white' }}>
+            <h1>Tu texto aquí</h1>
+            <h2>Hola</h2>
+            <h1>Tu texto aquí</h1>
+            <h2>Hola</h2>
+            <h1>Tu texto aquí</h1>
+            <h2>Hola</h2>
+            <h1>Tu texto aquí</h1>
+            <h2>Hola</h2>
+        </div>
       </>
-
     );
-  }
+}
   
 export default Home
   
