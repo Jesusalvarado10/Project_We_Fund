2b36c25485f684d581e9126a019530efd825dc60
Jesus <97445036+Jesusalvarado10@users.noreply.github.com>
2024-06-01 14:41:26 -0400

    ++Fixed PRIVATE_KEY firebase

--- backend/firebase/firebase.js
+++ backend/firebase/firebase.js
@@ -1,12 +1,11 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../config/serviceAccountKey.json'); // Asegúrate de que la ruta sea correcta
 require('dotenv').config();
 
 const serviceAccount = {
   type: process.env.GOOGLE_TYPE,
   project_id: process.env.GOOGLE_PROJECT_ID,
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.GOOGLE_PRIVATE_KEY,
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
   client_id: process.env.GOOGLE_CLIENT_ID,
   auth_uri: process.env.GOOGLE_AUTH_URI,
