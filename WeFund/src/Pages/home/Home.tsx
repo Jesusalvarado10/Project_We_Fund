be6976cbb5d63a66d20fcbd0bc543c198a393b7e
Jesus <97445036+Jesusalvarado10@users.noreply.github.com>
2024-06-20 05:26:47 -0400

    listo

--- WeFund/src/Pages/home/Home.tsx
+++ WeFund/src/Pages/home/Home.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
-import "./Home.css";
+
+
+import "./Home.css"   
 import op2 from '../../assets/op2.png';
 import bambi from '../../assets/bambi.png';
-import fundana from '../../assets/FUNDANA.jpg'
-import aplav from '../../assets/aplav.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserGraduate } from '@fortawesome/free-solid-svg-icons/faUserGraduate';
 import { faHeartPulse } from '@fortawesome/free-solid-svg-icons/faHeartPulse';
@@ -11,109 +10,96 @@ import { faBurger } from '@fortawesome/free-solid-svg-icons/faBurger';
 import { faShirt } from '@fortawesome/free-solid-svg-icons/faShirt';
 import { faFutbol } from '@fortawesome/free-solid-svg-icons/faFutbol';
 
-const fundaciones = [
-  {
-    img: bambi,
-    nombre: "Hogar Bambi Venezuela",
-    descripcion: "En Hogar Bambi brindamos atención integral a niños y niñas de 0 a 18 años, en nuestras cinco casas ubicadas en San Bernardino. Trabajamos por la infancia en estado de vulnerabilidad, proporcionando un entorno seguro, respetuoso y amoroso, mientras restituimos sus derechos, trabajando con las familias y la comunidad.",
-    recaudado: "$13.457,09 recaudados",
-    color: "#4CAF50"
-  },
-  {
-    img: fundana,
-    nombre: "Fundana",
-    descripcion: "Brindamos protección y acompañamiento a niños, niñas, mujeres y familias vulnerables. Nos aseguramos que cada niño sea reinsertado en un medio familiar seguro. Realizamos trabajo preventivo junto a las comunidades, atendiendo problemáticas familiares, alimentarias, de higiene y de violencia intrafamiliar...",
-    recaudado: "$8.547,71 recaudados",
-    color: "orange"
-  },
-  {
-    img: aplav,
-    nombre: "Amor Por Los Animales Venezuela (APLAV)",
-    descripcion: "La idea de consolidar nuestra fundación surge de la necesidad de generar una serie de actividades en pro del bienestar animal con la finalidad de contrarrestar el maltrato hacia los mismos en todas sus formas, especialmente el abandono.",
-    recaudado: "$5.201,34 recaudados",
-    color: "#00CED1"
-  },
 
-];
 
 function Home() {
-  const [currentFundacion, setCurrentFundacion] = useState(0);
-
-  const handlePrev = () => {
-    setCurrentFundacion((prev) => (prev === 0 ? fundaciones.length - 1 : prev - 1));
-  };
-
-  const handleNext = () => {
-    setCurrentFundacion((prev) => (prev === fundaciones.length - 1 ? 0 : prev + 1));
-  };
-
   return (
     <>
       <div className="bg-white shadow-md z-10 w-full" style={{ marginTop: '0.5rem' }}>
         <div className="grid grid-cols-5 items-center justify-center gap-4 py-2 md:py-3 px-4 md:px-8">
-          <a href="#" className="flex flex-col items-center text-gray-700 group">
-            <FontAwesomeIcon 
-              icon={faUserGraduate} 
-              className="text-4xl mb-2 group-hover:text-blue-500 transition-colors duration-300" 
-            />
-            <span className="group-hover:text-blue-500 transition-colors duration-300">Educación</span>
-          </a>
-          <a href="#" className="flex flex-col items-center text-gray-700 group">
-            <FontAwesomeIcon icon={faHeartPulse} 
-              className="text-4xl mb-2 group-hover:text-blue-500 transition-colors duration-300" 
-            />
-            <span className="group-hover:text-blue-500 transition-colors duration-300">Salud</span>
-          </a>
-          <a href="#" className="flex flex-col items-center text-gray-700 group">
-            <FontAwesomeIcon icon={faBurger} 
-              className="text-4xl mb-2 group-hover:text-blue-500 transition-colors duration-300" 
-            />
-            <span className="group-hover:text-blue-500 transition-colors duration-300">Alimentos</span>
-          </a>
-          <a href="#" className="flex flex-col items-center text-gray-700 group">
-            <FontAwesomeIcon icon={faShirt}  
-              className="text-4xl mb-2 group-hover:text-blue-500 transition-colors duration-300" 
-            />
-            <span className="group-hover:text-blue-500 transition-colors duration-300">Vestimenta</span>
-          </a>
-          <a href="#" className="flex flex-col items-center text-gray-700 group">
-            <FontAwesomeIcon icon={faFutbol} 
-              className="text-4xl mb-2 group-hover:text-blue-500 transition-colors duration-300" 
-            />
-            <span className="group-hover:text-blue-500 transition-colors duration-300">Deporte</span>
-          </a>
+      
+      <a href="#" className="flex flex-col items-center text-gray-700 group">
+        <FontAwesomeIcon 
+          icon={faUserGraduate} 
+          className="text-4xl mb-2 group-hover:text-blue-500 transition-colors duration-300" 
+        />
+      
+        <span className="group-hover:text-blue-500 transition-colors duration-300">
+          Educación
+        </span>
+      </a>
+      <a href="#" className="flex flex-col items-center text-gray-700 group">
+      <FontAwesomeIcon icon={faHeartPulse} 
+          className="text-4xl mb-2 group-hover:text-blue-500 transition-colors duration-300" 
+        />
+      
+        <span className="group-hover:text-blue-500 transition-colors duration-300">
+          Salud
+        </span>
+      </a>
+
+      <a href="#" className="flex flex-col items-center text-gray-700 group">
+      <FontAwesomeIcon icon={faBurger} 
+          className="text-4xl mb-2 group-hover:text-blue-500 transition-colors duration-300" 
+        />
+      
+        <span className="group-hover:text-blue-500 transition-colors duration-300">
+          Alimentos
+        </span>
+      </a>
+      <a href="#" className="flex flex-col items-center text-gray-700 group">
+      <FontAwesomeIcon icon={faShirt}  
+          className="text-4xl mb-2 group-hover:text-blue-500 transition-colors duration-300" 
+        />
+      
+        <span className="group-hover:text-blue-500 transition-colors duration-300">
+          Vestimenta
+        </span>
+      </a>
+      <a href="#" className="flex flex-col items-center text-gray-700 group">
+      <FontAwesomeIcon icon={faFutbol} 
+          className="text-4xl mb-2 group-hover:text-blue-500 transition-colors duration-300" 
+        />
+      
+        <span className="group-hover:text-blue-500 transition-colors duration-300">
+          Deporte
+        </span>
+      </a>
+
         </div>
       </div>
       <div className="relative h-screen flex items-center justify-center">
-        <img src={op2} alt="background" className="absolute w-screen h-screen object-cover z-0" />
+  <img src={op2} alt="background" className="absolute w-screen h-screen object-cover z-0 " />
+  {/* <div className="relative z-10 text-white text-center bg-black bg-opacity-70 p-4 rounded">
+    "Si ayudo a una sola persona a tener esperanza, no habré vivido en vano."
+  </div> */}
+
+</div>
+      <div>
+      
       </div>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-white-200">
-      <div className="flex justify-center w-full max-w-4xl mb-4">
-      <button onClick={handlePrev} className="bg-green-500 text-white px-10 py-2 rounded-full shadow-md mr-80" style={{ fontFamily: 'Kanit, sans-serif' }}>Anterior</button>
-      <button onClick={handleNext} className="bg-green-500 text-white px-10 py-2 rounded-full shadow-md ml-80" style={{ fontFamily: 'Kanit, sans-serif' }}>Siguiente</button>
-    </div>
-        <div className="relative bg-white shadow-lg rounded-lg p-6 w-full max-w-4xl" style={{ backgroundColor: fundaciones[currentFundacion].color }}>
-          <div className="w-full h-3/4 flex items-center justify-center rounded-lg p-6">
-            <div className="grid grid-cols-2 gap-4">
-              <div className="mt-10">
-                <img src={fundaciones[currentFundacion].img} alt="background" className="w-72 h-72 ml-10" />
-              </div>
-              <div className="mr-10 flex flex-col justify-between">
-                <div>
-                  <h4 className="text-4xl text-white">{fundaciones[currentFundacion].nombre}</h4>
-                  <p className="text-white">{fundaciones[currentFundacion].descripcion}</p>
-                </div>
-                <div className="flex justify-between mt-4">
-                  <button className="button mr-4">{fundaciones[currentFundacion].recaudado}</button>
-                  <button className="button">Donar</button>
-                </div>
-              </div>
-            </div>
+      <div className="absolute bg-green-500  w-full h-3/4 flex items-center justify-center rounded-lg" >
+        <div className='grid grid-cols-2 gap-4  '>
+        <div>
+        <img src={bambi} alt="background" className="w-72 h-72 ml-40"/>
+          </div>
+          <div className="mr-20">
+          <h4 className="text-4xl text-white">Hogar Bambi Venezuela</h4>
+          <p>.</p>
+          <h1 className = "text-white">En Hogar Bambi brindamos atención integral con enfoque sistémico a niños y niñas de 0 a 18 años, en nuestras cinco casas ubicadas en San Bernardino. Somos una organización que trabaja por la infancia en estado de vulnerabilidad, proporcionando un entorno seguro, respetuoso y amoroso, mientras restituimos sus derechos, trabajando con las familias y la comunidad.</h1>
+          <p>.</p>
+          <div>
+          <button className='button mr-4'>$13.457,09 recaudados</button>
+          <button className='button'>Donar</button>
+        </div>
+       
           </div>
         </div>
       </div>
+      
+      
     </>
   );
 }
 
-export default Home;
+export default Home
