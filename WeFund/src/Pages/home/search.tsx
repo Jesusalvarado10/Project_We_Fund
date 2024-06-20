e8ee7a03572c6c2ab1b3465f15e54e7cd9409740
andreapintoc <andrea.pinto@correo.unimet.edu.ve>
2024-06-20 12:02:59 -0400

    busqueda

--- WeFund/src/Pages/home/search.tsx
+++ WeFund/src/Pages/home/search.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TiZoom } from "react-icons/ti";
 import { useNavigate } from "react-router-dom";
 import { nationalURL } from "../../constants/url";
@@ -12,74 +12,75 @@ interface Foundation {
 }
 
 const Search = () => {
-const navigate = useNavigate();
-  const [query, setQuery] = useState('');
-  const [data1, setData1] = useState<Foundation[]>([]);
-const [foundations, setFoundations] = useState<Foundation[]>(data1);
+  const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+  const [foundations, setFoundations] = useState<Foundation[]>([]);
 
-const fetchFoundations = async () => {
-   
-    try {
-      const response = await fetch('https://project-we-fund-logic2-0.onrender.com/fundaciones');
-      if (!response.ok) {
-        throw new Error('Error fetching foundations');
+  useEffect(() => {
+    const fetchFoundations = async () => {
+      try {
+        const response = await fetch(
+          "https://project-we-fund-logic2-0.onrender.com/fundaciones"
+        );
+        if (!response.ok) {
+          throw new Error("Error fetching foundations");
+        }
+        const data = await response.json();
+        setFoundations(data.fundaciones);
+      } catch (error) {
+        console.error("Error fetching foundations:", error);
       }
-      const data = await response.json();
-
-
-      setData1(data.fundaciones);
-    } catch (error) {
-      console.error('Error fetching foundations:', error);
-    }
-  };
-  fetchFoundations();
-  
+    };
+    fetchFoundations();
+  }, []);
 
   const changeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
- 
     const searchTerm = e.target.value.toLowerCase();
     setQuery(searchTerm);
-    
-    const filteredFoundations = foundations.filter(foundation =>
-      foundation.tittle.toLowerCase().includes(searchTerm) || foundation.type.toLowerCase().includes(searchTerm)
+
+    const filteredFoundations = foundations.filter(
+      (foundation) =>
+        foundation.tittle.toLowerCase().includes(searchTerm) ||
+        foundation.type.toLowerCase().includes(searchTerm)
     );
     setFoundations(filteredFoundations);
-    if(searchTerm === '') {
-        setFoundations(data1);
-    
-    }
-  }
+  };
 
   return (
     <div className="flex flex-col items-center mt-6">
-      <div className="flex">
-        <input
-          type="text"
-          className="border border-gray-300 rounded-full px-4 py-1 focus:outline-none focus:border-blue-500 transition-colors duration-300"
-          value={query}
-          onChange={changeSearch}
-          placeholder="Buscar fundaciones..."
-        />
-        <TiZoom size={32} />
+      <div className="w-full max-w-md mb-6">
+        <div className="relative">
+          <input
+            type="text"
+            className="w-full border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:border-violet-500 transition-colors duration-300"
+            value={query}
+            onChange={changeSearch}
+            placeholder="Buscar fundaciones..."
+          />
+          <div className="absolute inset-y-0 right-0 flex items-center pr-4">
+            <TiZoom size={24} className="text-gray-400" />
+          </div>
+        </div>
       </div>
-      <div className="h-screen ">
-      <div className="mt-5 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {foundations.map(foundation => (
-          <div key={foundation.id} className="bg-white rounded-lg shadow-lg">
-            <img src={foundation.banner} alt="" className="w-full h-32 object-cover object-center" />
-            <div className="p-4">
-              <h3 className="text-xl font-semibold text-black">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-6xl">
+        {foundations.map((foundation) => (
+          <div
+            key={foundation.id}
+            className="bg-white rounded-lg shadow-lg overflow-hidden"
+          >
+            <img
+              src={foundation.banner}
+              alt=""
+              className="w-full h-48 object-cover object-center"
+            />
+            <div className="p-6">
+              <h3 className="text-xl font-semibold text-black mb-2">
                 {foundation.tittle}
               </h3>
-              <p>
-                {foundation.type}
-              </p>
-             
+              <p className="text-gray-600 mb-4">{foundation.type}</p>
               <button
-                className="mt-4 bg-violet-600 hover:bg-violet-700 text-white font-semibold py-2 px-4 rounded"
-                onClick={() => {
-                 navigate(nationalURL)
-                }}
+                className=" bg-green-500 hover:bg-[#0A2F23] text-white font-semibold py-2 px-4 rounded"
+                onClick={() => navigate(nationalURL)}
               >
                 Donar
               </button>
@@ -87,9 +88,8 @@ const fetchFoundations = async () => {
           </div>
         ))}
       </div>
-      </div>
     </div>
   );
-}
+};
 
 export default Search;
