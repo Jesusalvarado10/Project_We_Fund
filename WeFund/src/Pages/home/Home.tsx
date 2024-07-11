cf4264fe72fd3ec65911440be03a62acbc712bd6
Santiago <mcuervo@correo.unimet.edu.ve>
2024-07-11 01:25:05 -0400

    Avances

--- WeFund/src/Pages/home/Home.tsx
+++ WeFund/src/Pages/home/Home.tsx
@@ -131,9 +131,9 @@ const Home = () => {
 
     </div>
   </div>
-      <div className="relative h-screen flex items-center justify-center">
-        <img src={op2} alt="background" className="absolute w-screen h-screen object-cover z-0 opacity-70" />
-        <div className="relative z-10 text-white text-center bg-green-500 bg-opacity-70 p-4 rounded">
+      <div className="relative h-screen flex items-start justify-center">
+        <img src={op2} alt="background" className="absolute w-screen h-screen object-cover z-0 opacity-80" />
+        <div className="relative z-10 text-white text-center bg-green-500 bg-opacity-100 p-4 rounded px-4 my-12">
           "Uniendo corazones, multiplicando impactos"
         </div>
       </div>
@@ -141,20 +141,21 @@ const Home = () => {
 
       <div className="flex flex-col items-center justify-center w-full flex-1 px-20 py-10 text-center">
   <div className="bg-white shadow-md rounded-lg p-6 my-10">
-    <h2 className="text-2xl font-bold mb-4">¿Quiénes somos?</h2>
-
-    <p>WeFund es una innovadora plataforma de crowdfunding diseñada específicamente para ayudar a fundaciones a recaudar fondos de manera efectiva y transparente.</p>
-    <p>A través de WeFund, las fundaciones pueden crear campañas personalizadas, compartir sus historias y objetivos, y alcanzar a una amplia audiencia de potenciales donantes.</p>
-    <p>Para los donantes, WeFund proporciona una experiencia segura y transparente, permitiéndoles explorar diversas causas, realizar donaciones de forma sencilla</p>
-    <h5 className="font-bold">¡Conoce quienes somos, nuestra mision, vision y valores!</h5>
-    <button
-      className="mt-4 bg-green-500 hover:bg-[#0A2F23] text-white font-semibold py-2 px-4 rounded"
-      onClick={() => {
-        navigate(ideaURL)
-      }}
-    >
-      Ver mas
-    </button>
+  <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg">
+  <h2 className="text-3xl font-bold text-gray-800 mb-6">¿Quiénes somos?</h2>
+  <p className="text-gray-700 mb-4"> WeFund es una innovadora plataforma de crowdfunding diseñada específicamente para ayudar a fundaciones a recaudar fondos de manera efectiva y transparente.</p>
+  <p className="text-gray-700 mb-4"> A través de WeFund, las fundaciones pueden crear campañas personalizadas, compartir sus historias y objetivos, y alcanzar a una amplia audiencia de potenciales donantes.</p>
+  <p className="text-gray-700 mb-6">Para los donantes, WeFund proporciona una experiencia segura y transparente, permitiéndoles explorar diversas causas y realizar donaciones de forma sencilla.</p>
+  <h5 className="text-xl font-bold text-gray-800 mb-6">¡Conoce quienes somos, nuestra misión, visión y valores!</h5>
+  <button
+    className="mt-4 bg-green-500 hover:bg-[#0A2F23] text-white font-semibold py-2 px-6 rounded transition duration-300"
+    onClick={() => {
+      navigate(ideaURL)
+    }}
+  >
+    Ver más
+  </button>
+</div>
   </div>
 </div>
 <img
@@ -164,36 +165,40 @@ const Home = () => {
       />
       <div className="flex flex-col items-center mt-6">
       <div className="flex flex-col items-center mt-6">
-  <h2 className="text-2xl text-center font-bold mb-4">Algunas fundaciones</h2>
+  <h2 className="text-3xl text-center font-bold mb-8 text-gray-800">
+  <span className="inline-block bg-green-500 text-white py-2 px-6 rounded-full shadow-lg">
+    Algunas fundaciones
+  </span>
+  </h2>
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-      {fundaciones.map(foundation => (
-        <div key={foundation.id} className="bg-white rounded-lg shadow-lg flex flex-col">
-          <img src={foundation.photo} alt={foundation.tittle} className="w-full h-48 object-cover object-center rounded-t-lg" />
-          <div className="p-4 flex-grow">
-            <h3 className="text-xl font-semibold text-black mb-2">{foundation.tittle}</h3>
-            <p className="text-gray-600 mb-4">{foundation.type}</p>
-            <div className="mt-auto flex justify-between">
-              <button
-                className="bg-green-500 hover:bg-[#0A2F23] text-white font-semibold py-2 px-4 rounded transition duration-300"
-                onClick={() => {
-                  // Add donation logic here
-                }}
-              >
-                Donar
-              </button>
-              <button
-                className="bg-green-500 hover:bg-[#0A2F23] text-white font-semibold py-2 px-4 rounded transition duration-300"
-                onClick={() => navigate(`/fundations/${foundation.id}`)}
-              >
-                Ver más	
-              </button>
-            </div>
+  <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+    {fundaciones.map(foundation => (
+      <div key={foundation.id} className="bg-white rounded-lg shadow-lg flex flex-col hover:shadow-xl transition-shadow duration-300">
+        <img src={foundation.photo} alt={foundation.tittle} className="w-full h-48 object-cover object-center rounded-t-lg" />
+        <div className="p-6 flex flex-col flex-grow">
+          <h3 className="text-xl font-semibold text-black mb-3">{foundation.tittle}</h3>
+          <p className="text-gray-600 mb-6">{foundation.type}</p>
+          <div className="mt-auto flex justify-between">
+            <button
+              className="bg-green-500 hover:bg-[#0A2F23] text-white font-semibold py-2 px-4 rounded transition duration-300"
+              onClick={() => {
+                // Lógica para donar
+              }}
+            >
+              Donar
+            </button>
+            <button
+              className="bg-green-500 hover:bg-[#0A2F23] text-white font-semibold py-2 px-4 rounded transition duration-300"
+              onClick={() => navigate(`/fundations/${foundation.id}`)}
+            >
+              Ver más
+            </button>
           </div>
         </div>
-      ))}
-    </div>
+      </div>
+    ))}
   </div>
+</div>
 </div>
     </div>
     
